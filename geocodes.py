6c8ed5416e837eb948fd1c40c577bb78c5b8d0ec
TechOtakuW <88263897+TechOtakuW@users.noreply.github.com>
2022-03-14 23:30:21 +0800

    Update geocodes.py

--- geocodes.py
+++ geocodes.py
@@ -1,7 +1,7 @@
 import requests #用于API HTTP requests
 #使用python字典构造 parameters 请求函数
 #API使用请求函数
-key = '1fe97c7203cd1a04110ec439d230083e' #高德地图API 获取key
+key = 'xxxxxxxxxxxxxxxxxxxxxxxx' #高德地图API 获取key
 def geo(address:str,city=None)-> dict:
     """获取地理编码
     address可填入国家、省份、城市、区县、城镇、乡村、街道、门牌号码等名称
